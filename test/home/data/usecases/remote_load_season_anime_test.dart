060e09adad72f16f202ec0949d45b6425fad752f
Rodrigo Anaice <rodrigoanaice@hotmail.com>
2021-09-12 23:20:29 -0300

    Move sut and instantiation to setUp method

--- test/home/data/usecases/remote_load_season_anime_test.dart
+++ test/home/data/usecases/remote_load_season_anime_test.dart
@@ -12,12 +12,16 @@ import 'remote_load_season_anime_test.mocks.dart';
 
 @GenerateMocks([RemoteClient])
 void main() {
-  setUp(() {});
+  late RemoteLoadUpcomingAnimes sut;
+  late MockRemoteClient<LoadFailure, List<UpcomingAnimeEntity>> remoteClient;
+  late String path;
+  setUp(() {
+    path = faker.internet.httpsUrl();
+    remoteClient = MockRemoteClient();
+    sut = RemoteLoadUpcomingAnimes(path: path, remoteClient: remoteClient);
+  });
 
   test('Should call RemoteClient with correct params', () async {
-    final path = faker.internet.httpsUrl();
-    final remoteClient = MockRemoteClient<LoadFailure, List<UpcomingAnimeEntity>>();
-    final sut = RemoteLoadUpcomingAnimes(path: path, remoteClient: remoteClient);
     when(remoteClient.request(path: anyNamed('path'), method: anyNamed('method'), body: anyNamed('body')))
         .thenAnswer((realInvocation) async => right(<UpcomingAnimeEntity>[]));
 
