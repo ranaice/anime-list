09bb068aa5eac84dca32727031442db75e87d10f
Rodrigo Anaice <rodrigoanaice@hotmail.com>
2021-09-13 02:05:33 -0300

    Swap mockito for mocktail

--- test/home/data/usecases/remote_load_upcoming_anime_test.dart
+++ test/home/data/usecases/remote_load_upcoming_anime_test.dart
@@ -1,22 +1,30 @@
 import 'package:anime_list/core/data/remote_client.dart';
 import 'package:anime_list/home/data/usecases/remote_load_upcoming_animes.dart';
 import 'package:anime_list/home/domain/entities/upcoming_anime_entity.dart';
-import 'package:anime_list/home/domain/helpers/load_failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/annotations.dart';
-import 'package:mockito/mockito.dart';
+import 'package:mocktail/mocktail.dart';
 
-import 'remote_load_upcoming_anime_test.mocks.dart';
-
-@GenerateMocks([RemoteClient])
 void main() {
   late RemoteLoadUpcomingAnimes sut;
-  late MockRemoteClient<LoadFailure, List<UpcomingAnimeEntity>> remoteClient;
+  late MockRemoteClient remoteClient;
   late String path;
 
-  List<UpcomingAnimeEntity> upcomingAnimes = [];
+  List<Map<String, dynamic>> mockValidData = [
+    {
+      "mal_id": faker.randomGenerator.integer(10000),
+      "title": faker.person.name(),
+      "image_url": faker.internet.httpUrl(),
+      "start_date": "Apr 2022"
+    },
+    {
+      "mal_id": faker.randomGenerator.integer(10000),
+      "title": faker.person.name(),
+      "image_url": faker.internet.httpUrl(),
+      "start_date": "Apr 2022"
+    },
+  ];
 
   setUp(() {
     path = faker.internet.httpsUrl();
@@ -25,11 +33,13 @@ void main() {
   });
 
   test('Should call RemoteClient with correct params', () async {
-    when(remoteClient.request(path: anyNamed('path'), method: anyNamed('method')))
+    when(() => remoteClient.request(path: any(named: 'path'), method: any(named: 'method')))
         .thenAnswer((realInvocation) async => right(<UpcomingAnimeEntity>[]));
 
     await sut.load();
 
-    verify(remoteClient.request(path: path, method: 'get'));
+    verify(() => remoteClient.request(path: path, method: 'get'));
   });
 }
+
+class MockRemoteClient extends Mock implements RemoteClient {}
