dbad031a3dc64de95704b276149f64379232a4c9
Rodrigo Anaice <rodrigoanaice@hotmail.com>
2021-09-14 12:36:18 -0300

    Test RemoteLoadUpcomingAnimes data layer

--- test/home/data/usecases/remote_load_upcoming_anime_test.dart
+++ test/home/data/usecases/remote_load_upcoming_anime_test.dart
@@ -1,44 +1,72 @@
 import 'package:anime_list/core/data/remote_client.dart';
+import 'package:anime_list/core/data/remote_failure.dart';
 import 'package:anime_list/home/data/usecases/remote_load_upcoming_animes.dart';
-import 'package:anime_list/home/domain/entities/upcoming_anime_entity.dart';
+import 'package:anime_list/home/domain/helpers/load_failure.dart';
 import 'package:dartz/dartz.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+
+import 'upcoming_anime_fixture.dart';
 
 void main() {
   late RemoteLoadUpcomingAnimes sut;
   late MockRemoteClient remoteClient;
   late String path;
 
-  List<Map<String, dynamic>> mockValidData = [
-    {
-      "mal_id": faker.randomGenerator.integer(10000),
-      "title": faker.person.name(),
-      "image_url": faker.internet.httpUrl(),
-      "start_date": "Apr 2022"
-    },
-    {
-      "mal_id": faker.randomGenerator.integer(10000),
-      "title": faker.person.name(),
-      "image_url": faker.internet.httpUrl(),
-      "start_date": "Apr 2022"
-    },
-  ];
+  When mockRequest() => when(() => remoteClient.request(path: any(named: 'path'), method: any(named: 'method')));
+
+  void mockSuccessResponse(Map<String, dynamic>? data) {
+    mockRequest().thenAnswer((_) async => right<RemoteFailure, Map<String, dynamic>?>(data));
+  }
+
+  void mockFailure(RemoteFailure remoteFailure) => mockRequest().thenAnswer(
+        (invocation) async => left<RemoteFailure, Map<String, dynamic>?>(remoteFailure),
+      );
 
   setUp(() {
     path = faker.internet.httpsUrl();
     remoteClient = MockRemoteClient();
     sut = RemoteLoadUpcomingAnimes(path: path, remoteClient: remoteClient);
+    mockSuccessResponse(rawUpcomingResponse);
   });
 
   test('Should call RemoteClient with correct params', () async {
-    when(() => remoteClient.request(path: any(named: 'path'), method: any(named: 'method')))
-        .thenAnswer((realInvocation) async => right(<UpcomingAnimeEntity>[]));
-
     await sut.load();
 
-    verify(() => remoteClient.request(path: path, method: 'get'));
+    verify(() => remoteClient.request<Map<String, dynamic>>(path: path, method: 'get'));
+  });
+
+  test('Should throw LoadFailure.server() if HttpClient returns 400', () async {
+    mockFailure(const RemoteFailure.badRequest());
+
+    final failure = await sut.load();
+
+    expect(failure, left(const LoadFailure.server()));
+  });
+
+  test('Should throw LoadFailure.server() if HttpClient returns 404', () async {
+    mockFailure(const RemoteFailure.notFound());
+
+    final failure = await sut.load();
+
+    expect(failure, left(const LoadFailure.server()));
+  });
+
+  test('Should throw LoadFailure.server() if HttpClient returns 429', () async {
+    mockFailure(const RemoteFailure.tooManyRequest());
+
+    final failure = await sut.load();
+
+    expect(failure, left(const LoadFailure.server()));
+  });
+
+  test('Should throw LoadFailure.server() if HttpClient returns 500 or 503', () async {
+    mockFailure(const RemoteFailure.server());
+
+    final failure = await sut.load();
+
+    expect(failure, left(const LoadFailure.server()));
   });
 }
 
---
+++ test/home/data/usecases/upcoming_anime_fixture.dart
@@ -0,0 +1,50 @@
+import 'package:faker/faker.dart';
+
+List<Map<String, dynamic>> mockValidData = [
+  {
+    "mal_id": faker.randomGenerator.integer(10000),
+    "title": faker.person.name(),
+    "image_url": faker.internet.httpUrl(),
+    "start_date": "Apr 2022"
+  },
+  {
+    "mal_id": faker.randomGenerator.integer(10000),
+    "title": faker.person.name(),
+    "image_url": faker.internet.httpUrl(),
+    "start_date": "Apr 2022"
+  },
+];
+
+Map<String, dynamic> get rawUpcomingResponse => {
+      "request_hash": "request:top:3506eaba6445f7ad5cc2f78417bf6ed916b6aaad",
+      "request_cached": true,
+      "request_cache_expiry": 152331,
+      "top": [
+        {
+          "mal_id": 40356,
+          "rank": 1,
+          "title": "Tate no Yuusha no Nariagari Season 2",
+          "url": "https://myanimelist.net/anime/40356/Tate_no_Yuusha_no_Nariagari_Season_2",
+          "image_url": "https://cdn.myanimelist.net/images/anime/1245/111800.jpg?s=7302aaeb3bc4e1433b32d094e9d6f6f0",
+          "type": "TV",
+          "episodes": {},
+          "start_date": "Apr 2022",
+          "end_date": {},
+          "members": 327048,
+          "score": 0
+        },
+        {
+          "mal_id": 48583,
+          "rank": 2,
+          "title": "Shingeki no Kyojin: The Final Season Part 2",
+          "url": "https://myanimelist.net/anime/48583/Shingeki_no_Kyojin__The_Final_Season_Part_2",
+          "image_url": "https://cdn.myanimelist.net/images/anime/1989/116577.jpg?s=f6312bda2e67f86595936d0264696a91",
+          "type": "TV",
+          "episodes": {},
+          "start_date": "Jan 2022",
+          "end_date": {},
+          "members": 303634,
+          "score": 0
+        },
+      ],
+    };
