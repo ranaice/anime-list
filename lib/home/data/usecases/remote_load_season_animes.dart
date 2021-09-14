b6e8cc7d84e6a7520afe810ae47e2a1eb3a965ac
Rodrigo Anaice <rodrigoanaice@hotmail.com>
2021-09-13 23:45:15 -0300

    Add remote load logic

---
+++ lib/home/data/usecases/remote_load_season_animes.dart
@@ -0,0 +1,31 @@
+import 'package:dartz/dartz.dart';
+
+import '../../../core/data/remote_client.dart';
+import '../../domain/entities/season_anime_entity.dart';
+import '../../domain/helpers/load_failure.dart';
+import '../../domain/usecases/load_season_animes.dart';
+import '../models/season_anime_model.dart';
+
+class RemoteLoadSeasonAnimes extends LoadSeasonAnimes {
+  final RemoteClient remoteClient;
+  final String path;
+
+  RemoteLoadSeasonAnimes({required this.remoteClient, required this.path});
+
+  @override
+  Future<Either<LoadFailure, List<SeasonAnimeEntity>>> load() async {
+    final failureOrResponse = await remoteClient.request<Map<String, dynamic>>(
+      path: path,
+      method: 'get',
+    );
+
+    return failureOrResponse.fold(
+      (l) => left(const LoadFailure.server()),
+      (r) {
+        final onlyTop = r?['top'] as List;
+        final animes = onlyTop.map((e) => SeasonAnimeModel.fromJson(e as Map<String, dynamic>).toEntity()).toList();
+        return right(animes);
+      },
+    );
+  }
+}
