d170762ea2d62f61dacba5158a37abe5af6e053a
Rodrigo Anaice <rodrigoanaice@hotmail.com>
2021-09-13 23:00:37 -0300

    Add common home widgets

---
+++ lib/home/presentation/widgets/common/failure_layout.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+import '../../../../core/presentation/l10n/l10n_extensions.dart';
+import '../../../../core/presentation/themes/styles.dart';
+
+class FailureLayout extends StatelessWidget {
+  final void Function() onRetry;
+
+  const FailureLayout({Key? key, required this.onRetry}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          const Icon(
+            Icons.offline_bolt_rounded,
+            size: IconSizes.huge,
+          ),
+          const SizedBox(height: Dimensions.med),
+          Text(
+            context.l10n.errorMessage,
+            textAlign: TextAlign.center,
+            style: Theme.of(context).textTheme.bodyText2!.copyWith(fontWeight: FontWeight.w600),
+          ),
+          const SizedBox(height: Dimensions.med),
+          ConstrainedBox(
+            constraints: const BoxConstraints(minWidth: 150),
+            child: ElevatedButton(
+              onPressed: onRetry,
+              child: Text(context.l10n.tryAgain),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/home/presentation/widgets/common/section_header.dart
@@ -0,0 +1,15 @@
+import 'package:anime_list/core/presentation/l10n/l10n_extensions.dart';
+import 'package:flutter/material.dart';
+
+class SectionHeader extends StatelessWidget {
+  const SectionHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      context.l10n.upcomingAnimes,
+      textAlign: TextAlign.start,
+      style: Theme.of(context).textTheme.headline6,
+    );
+  }
+}
